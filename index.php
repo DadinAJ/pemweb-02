58cbc091579a9e5fd4429fd5afe9af8dc3a7da23
DadinAJ <dadinaj4@gmail.com>
2023-02-21 15:03:11 +0700

    dasar-dasar pemograman php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = 'Dadin Ahmad Jamaludin';
+    $umur = 1000;
+    $berat = 55;
+
+    echo 'Nama saya :' .$nama;
+    echo '<br><hr>Umur saya :' .$umur .' tahun'; 
+    echo '<br><hr>Berat saya :' .$berat .' Kg';
+?>
